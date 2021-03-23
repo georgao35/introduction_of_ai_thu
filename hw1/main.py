f4e538fab61bc7dddfb10896247d9e41a2a86db7
gjy <gaojy19@mails.tsinghua.edu.cn>
2021-03-23 22:17:56 +0800

    fix bugs

--- hw1/main.py
+++ hw1/main.py
@@ -1,10 +1,51 @@
 import argparse
-from train_2 import train
+import os
+from train_2 import train, Predictor
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file")
+    parser.add_argument("output_file")
+    parser.add_argument("--train")
+    input_path = parser.parse_args().input_file
+    output_path = parser.parse_args().output_file
+    try:
+        with open(input_path, 'r', encoding='gbk') as f:
+            lines = f.readlines()
+            predictor = Predictor()
+            with open(output_path, 'w', encoding='gbk') as out:
+                for line in lines:
+                    out.write(predictor.predict(line) + '\n')
+    except SystemError as e:
+        pass
+
+
+def test():
+    total = 0
+    total_correct = 0
+    total_correct_sen = 0
+    with open('输入法测试集.txt', 'r', encoding='utf-8') as f:
+        while True:
+            try:
+                input_line = f.readline()
+                if not input_line:
+                    break
+                while not input_line[-1].isprintable():
+                    input_line = input_line[:-1]
+                length = input_line.split(' ').__len__()
+                result = f.readline()
+                guess = predictor.predict(input_line)
+                for i in range(length):
+                    total += 1
+                    if guess[i] == result[i]:
+                        total_correct += 1
+            except Exception as e:
+                pass
+    print(total, total_correct, total_correct/total)
 
 
 if __name__ == '__main__':
-    pass
+    # main()
+    predictor = Predictor()
+    test()
--- hw1/train_2.py
+++ hw1/train_2.py
@@ -1,8 +1,8 @@
-import sys
+import os
 import json
 import re
-import numpy as np
-
+import math
+import pickle
 
 path_map = 'train/map.txt'
 path_stats = 'train/stats.txt'
@@ -21,7 +21,6 @@ def load_characters():
     all_cc_table = dict()
     with open('拼音汉字表_12710172/拼音汉字表.txt', 'r', encoding='gbk') as f:
         lines = f.readlines()
-        pinyin_num = lines.__len__() + 1
         character_num = 0
         pi_table['$'] = 0
         all_c_table['$'] = 0
@@ -41,19 +40,25 @@ def load_characters():
     return pc_table, cp_table, pi_table, ci_table, all_c_table, all_cc_table
 
 
-def load_news(index):
+def load_news(index, ci_table):
     """
     加载新闻
     :param index: 新闻文件的序号
     :return: 返回一个新闻内容的列表
     """
-    re_exp = '[^\u4e00-\u9fff]+'
     contents = []
     try:
         with open('sina_news_gbk/2016-%d.txt' % index, 'r', encoding='gbk') as f:
             lines = f.readlines()
             for line in lines:
-                contents.append(re.sub(re_exp, '$', json.loads(line)['html']))
+                src = json.loads(line)['html']
+                src = ''.join(
+                    word if word in ci_table else '$'
+                    for word in src
+                )
+                src = f'${src}$'
+                src = re.sub(r'(\$)\1+', '$', src)
+                contents.append(src)
     except SystemError as e:
         pass
     return contents
@@ -74,44 +79,41 @@ def load_results():
             all_c_table = json.loads(f.readline())
             all_cc_table = json.loads(f.readline())
         return pc_table, cp_table, pi_table, ci_table, all_c_table, all_cc_table
-    except SystemError as e:
+    except FileNotFoundError as e:
         return load_characters()
 
 
-def collect(pc_table, cp_table, pi_table, ci_table, all_c_table, all_cc_table,
-            p_freq=None, pp_freq=None, c_freq=None, cc_freq=None):
+def collect(pc_table, cp_table, pi_table, ci_table, all_c_table, all_cc_table):
     """
     在已有的结果上继续收集数据进行训练
+    :param ci_table:
+    :param pc_table:
+    :param cp_table:
+    :param pi_table:
     :param all_c_table: 每个汉字出现的次数
     :param all_cc_table: 每两个汉字组合出现的次数（如果未出现则不出现）
-    :param p_freq:
-    :param pp_freq:
-    :param c_freq:
-    :param cc_freq:
     :return: nothing
     """
     strings = []
     news_ids = [2, 4, 5, 6, 7, 8, 9, 10, 11]
     for i in news_ids:
-        strings += load_news(i)
+        strings += load_news(i, ci_table)
     for string in strings:
-        string = ''.join(['$', string])
-        for i, char in enumerate(string[1:]):
+        for i in range(len(string[1:])):
             try:
+                char = string[i]
                 all_c_table[char] += 1
-                chars = string[i-1] + char
-                if all_cc_table.__contains__(chars):
+                chars = string[i - 1] + char
+                if chars in all_cc_table:
                     all_cc_table[chars] += 1
                 else:
                     all_cc_table[chars] = 1
             except KeyError as e:
                 pass
-    save(pc_table, cp_table, pi_table, ci_table, all_c_table, all_cc_table,
-         p_freq, pp_freq, c_freq, cc_freq)
+    save(pc_table, cp_table, pi_table, ci_table, all_c_table, all_cc_table)
 
 
-def save(pc_table, cp_table, pi_table, ci_table, all_c_table, all_cc_table,
-         p_freq=None, c_freq=None, pp_freq=None, cc_freq=None):
+def save(pc_table, cp_table, pi_table, ci_table, all_c_table, all_cc_table):
     with open(path_map, 'w', encoding='utf-8') as f:
         f.write(json.dumps(pc_table, ensure_ascii=False) + '\n')
         f.write(json.dumps(cp_table, ensure_ascii=False) + '\n')
@@ -120,20 +122,67 @@ def save(pc_table, cp_table, pi_table, ci_table, all_c_table, all_cc_table,
     with open(path_stats, 'w', encoding='utf-8') as f:
         f.write(json.dumps(all_c_table, ensure_ascii=False) + '\n')
         f.write(json.dumps(all_cc_table, ensure_ascii=False) + '\n')
-    if pp_freq:
-        np.savetxt('train/p_freq.txt', p_freq)
-        np.savetxt('train/c_freq.txt', c_freq)
-        np.savetxt('train/pp_freq.txt', pp_freq)
-        np.savetxt('train/cc_freq.txt', cc_freq)
 
 
 def train():
     pc_table, cp_table, pi_table, ci_table, all_c_table, all_cc_table = load_results()
-    # p_freq = np.zeros(pinyin_num, dtype=np.int)
-    # pp_freq = np.zeros((pinyin_num, pinyin_num), dtype=np.int)
-    # c_freq = np.zeros(character_num, dtype=np.int)
-    # cc_freq = np.zeros((character_num, character_num), dtype=np.int)
     collect(pc_table, cp_table, pi_table, ci_table, all_c_table, all_cc_table)
+    return pc_table, cp_table, pi_table, ci_table, all_c_table, all_cc_table
+
+
+class Predictor:
+    def __init__(self):
+        self.params = dict()
+        self.record = list()
+        pc_table, cp_table, pi_table, ci_table, all_c_table, all_cc_table = load_results()
+        self.params['all_cc'] = all_cc_table
+        self.params['all_c'] = all_c_table
+        self.params['pc'] = pc_table
+        self.params['pc']['$'] = '$'
+        self.lam = 0.9
+        self.all_c_num = sum(self.params['all_c'].values())
+        print(self.all_c_num)
+        pass
+
+    def predict(self, line):
+        line = f'$ {line}'
+        pinyins = line.split(' ')
+        result = ''
+        all_cc = self.params['all_cc']
+        all_c = self.params['all_c']
+        self.record = []
+        print(pinyins)
+        for i in range(len(pinyins[:-1])):
+            pinyin = pinyins[i]
+            pinyin = pinyin.lower()
+            next_py = pinyins[i + 1]
+            self.record.append({})
+            if i:
+                for nxt_char in self.params['pc'][next_py]:
+                    self.record[i][nxt_char] = {'prev': '', 'p': math.inf}
+                    for now_char in self.params['pc'][pinyin]:
+                        word = now_char + nxt_char
+                        if word not in all_cc or now_char not in all_c:
+                            continue
+                        log_prob = \
+                            -math.log(all_cc[word] / all_c[now_char]) + self.record[i - 1][now_char]['p']
+                        if log_prob < self.record[i][nxt_char]['p']:
+                            self.record[i][nxt_char]['p'] = log_prob
+                            self.record[i][nxt_char]['prev'] = now_char
+            else:
+                for character in self.params['pc'][next_py]:
+                    log_prob = -math.log((all_c[character] + 1) / self.all_c_num)
+                    self.record[i][character] = {'prev': '$', 'p': log_prob}
+        char = sorted(self.record[-1].items(), key=lambda item: item[1]['p'])
+        char = char[0][0]
+        i = self.record.__len__() - 1
+        while char != '$':
+            print(sorted(self.record[i].items(), key=lambda item: item[1]['p']))
+            result = char + result
+            char = self.record[i][char]['prev']
+            i -= 1
+        print(result)
+        return result
 
 
 if __name__ == '__main__':
